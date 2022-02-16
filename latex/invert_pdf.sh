9a1c630561beca6bcdb798c72c4c78682791f909
gioele_migno <migno.1795826@studenti.uniroma1.it>
2022-02-16 17:15:24 +0100

    renamed script

---
+++ latex/invert_pdf.sh
@@ -0,0 +1,10 @@
+inotifywait -e close_write,moved_to,create -m . |
+while read -r directory events filename; do
+  if [ "$filename" = "0_main.pdf" ]; then
+    echo 'MODIFIED'
+pdftoppm 0_main.pdf __src -png -rx 300 -ry 300 && convert __src-*.png -channel RGB -negate -set filename:f '%t' ./'%[filename:f].png' && pdfjam __src-*.png --outfile dark_output.pdf && rm __src-*.png
+  fi
+done
+
+
+
